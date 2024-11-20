81a8ab8a322d87f3bffd6365808b3b4bb630dce5
FEI-Link <wang.2260587199@gmail.com>
2024-11-20 14:11:24 +0800

    转换思路

--- stash-develop/ui/v2.5/src/plugins.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { PatchFunction } from "./patch";
-
-export const PluginRoutes: React.FC<React.PropsWithChildren<{}>> =
-  PatchFunction("PluginRoutes", (props: React.PropsWithChildren<{}>) => {
-    return <>{props.children}</>;
-  }) as React.FC;
---
+++ xgplayer-main/packages/xgplayer/src/index.umd.js
@@ -0,0 +1,28 @@
+import Player from './player'
+import Plugin from './plugin/plugin'
+import BasePlugin from './plugin/basePlugin'
+import * as Events from './events'
+import STATE_CLASS from './stateClassMap'
+import I18N from './lang/i18n'
+import Errors from './error'
+import Sniffer from './utils/sniffer'
+import Util from './utils/util'
+import { InstManager } from './instManager'
+import './index.scss'
+
+import defaultPreset from './presets/default'
+
+class PresetPlayer extends Player {
+  static defaultPreset = defaultPreset;
+  static Util = Util;
+  static Sniffer = Sniffer
+  static Errors = Errors
+  static Events = Events
+  static Plugin = Plugin
+  static BasePlugin = BasePlugin
+  static I18N = I18N
+  static STATE_CLASS = STATE_CLASS
+  static InstManager = InstManager
+}
+
+export default PresetPlayer
